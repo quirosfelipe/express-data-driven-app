ef5f6497e1825242d2d9a1333c766486afae3d9d
Felipe Quiros <quirosfelipe@gmail.com>
2020-05-07 14:21:49 -0400

    basic setup

--- app.js
+++ app.js
@@ -1,15 +1,52 @@
 const express = require("express");
-
+const morgan = require("morgan");
 const routes = require("./routes");
 
 const app = express();
-
+app.use(morgan("dev"));
 app.set("view engine", "pug");
 
-app.use(routes);
+app.use(routes); // how this is connected to line 5 in routes.js
 
-// Define a port and start listening for connections.
+// Catch unhandled requests and forward to error handler.
+app.use((req, res, next) => {
+  const err = new Error("The requested page couldn't be found.");
+  err.status = 404;
+  next(err);
+});
+// Custom error handlers.
 
-const port = 8080;
+// Error handler to log errors.
+app.use((err, req, res, next) => {
+  if (process.env.NODE_ENV === "production") {
+    // TODO Log the error to the database.
+  } else {
+    console.error(err);
+  }
+  next(err);
+});
 
-app.listen(port, () => console.log(`Listening on port ${port}...`));
+// Error handler for 404 errors.
+app.use((err, req, res, next) => {
+  if (err.status === 404) {
+    res.status(404);
+    res.render("page-not-found", {
+      title: "Page Not Found",
+    });
+  } else {
+    next(err);
+  }
+});
+
+// Generic error handler.
+app.use((err, req, res, next) => {
+  res.status(err.status || 500);
+  const isProduction = process.env.NODE_ENV === "production";
+  res.render("error", {
+    title: "Server Error",
+    message: isProduction ? null : err.message,
+    stack: isProduction ? null : err.stack,
+  });
+});
+
+module.exports = app;
--- routes.js
+++ routes.js
@@ -2,6 +2,11 @@ const express = require("express");
 
 const router = express.Router();
 
+// router.get("/", (req, res) => {
+//   throw new Error("This is a test error!");
+//   res.render("index", { title: "Home" });
+// });
+
 router.get("/", (req, res) => {
   res.render("index", { title: "Home" });
 });
